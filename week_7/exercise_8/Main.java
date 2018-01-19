a7c7483b58b345994abdad404ef72ff9e3f92581
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-18 22:42:30 -0500

    exercise #8

---
+++ week_7/exercise_8/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        UserInterface airport = new UserInterface();
+        airport.start();
+    }
+}
---
+++ week_7/exercise_8/Plane.java
@@ -0,0 +1,21 @@
+public class Plane {
+    private String id;
+    private int capacity;
+    
+    public Plane(String id, int capacity){
+        this.id = id;
+        this.capacity = capacity;
+    }
+    
+    public String getId(){
+        return this.id;
+    }
+    
+    public int getCapacity(){
+        return this.capacity;
+    }
+    
+    public String toString(){
+        return this.id + " (" + this.capacity + " ppl)";
+    }
+}
